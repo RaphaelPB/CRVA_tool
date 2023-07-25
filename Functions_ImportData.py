ffb6705e776aab873f6529c7b292548572bc8f27
CDross0507 <128394382+CDross0507@users.noreply.github.com>
2023-07-25 16:32:11 +0200

    Work on BC

--- Functions_ImportData.py
+++ Functions_ImportData.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[1]:
 
 
 import pandas as pd
@@ -13,7 +13,7 @@ from Functions_Indicators import str_month
 from Functions_Indicators import add_year_month_season
 
 
-# In[6]:
+# In[2]:
 
 
 def treat_NOAA_data(daily_sum_obs_from_NOAA):
@@ -30,13 +30,13 @@ def treat_NOAA_data(daily_sum_obs_from_NOAA):
     return daily_sum_obs_from_NOAA
 
 
-# In[7]:
+# In[3]:
 
 
 def import_treat_modeled_NEX_GDDP_CMIP6_close_to_stationNOAA(climate_var, unit):
     # import data
     
-    path_NEX_GDDP_CMIP6_EmplacementStation=os.path.join(r'\\COWI.net\projects\A245000\A248363\CRVA\Datasets\NEX-GDDP-CMIP6-AllMoz\csv_file',climate_var,climate_var+'_'+unit+'_day_1970-2014_closest_point_to_NOAA','NEXGDDPCMIP6_at_same_emplacement_as_NOAA_stationPembaChimoioBeira_'+climate_var+'_1970-2014_projectsMoz.csv')
+    path_NEX_GDDP_CMIP6_EmplacementStation=os.path.join(r'\\COWI.net\projects\A245000\A248363\CRVA\Datasets\NEX-GDDP-CMIP6-AllMoz\csv_file',climate_var,climate_var+'_'+unit+'_day_1970-2014_Closest_to_NOAA','NEXGDDPCMIP6_at_same_emplacement_as_NOAA_stationPembaChimoioBeira_'+climate_var+'_1970-2014_projectsMoz.csv')
     
     data_NEX_GDDP_CMIP6_EmplacementStation = pd.read_csv(path_NEX_GDDP_CMIP6_EmplacementStation)
     
@@ -45,19 +45,46 @@ def import_treat_modeled_NEX_GDDP_CMIP6_close_to_stationNOAA(climate_var, unit):
     return data_NEX_GDDP_CMIP6_EmplacementStation
 
 
-# In[8]:
+# In[4]:
 
 
 def import_treat_modeled_NEX_GDDP_CMIP6(climate_var, unit,temporal_resolution,start_y,stop_y):
     # import data
     
-    path_NEX_GDDP_CMIP6=os.path.join(r'\\COWI.net\projects\A245000\A248363\CRVA\Datasets\NEX-GDDP-CMIP6-AllMoz\csv_file',climate_var,climate_var+'_'+unit+'_'+temporal_resolution+'_'+str(start_y)+'-'+str(stop_y),climate_var+'_'+str(start_y)+'-'+str(stop_y)+'_projectsMoz.csv')
-    
+    if climate_var =='pr':
+        path_NEX_GDDP_CMIP6=os.path.join(r'\\COWI.net\projects\A245000\A248363\CRVA\Datasets\NEX-GDDP-CMIP6-AllMoz\csv_file',climate_var,climate_var+'_'+unit+'_'+temporal_resolution+'_'+str(start_y)+'-'+str(stop_y),climate_var+'_'+str(start_y)+'-'+str(stop_y)+'_projectsMoz_before_25.07.csv')
+    else: # temperature
+        path_NEX_GDDP_CMIP6=os.path.join(r'\\COWI.net\projects\A245000\A248363\CRVA\Datasets\NEX-GDDP-CMIP6-AllMoz\csv_file',climate_var,climate_var+'_Celsius_day_1950-2100',climate_var+'_1950-2100_projectsMoz_before_25.07.csv')
+        
     data_NEX_GDDP_CMIP6 = pd.read_csv(path_NEX_GDDP_CMIP6)
     
     data_NEX_GDDP_CMIP6 = add_year_month_season(data_NEX_GDDP_CMIP6,'Date')
     
     return data_NEX_GDDP_CMIP6
+
+
+# In[5]:
+
+
+# this function is meant to import the NOAA observation data
+def import_treat_obs_NOAA():
+    # path where the file is placed
+    path_file_NOAA = r'C:\Users\CLMRX\COWI\A248363 - Climate analysis - Documents\General\CRVA_tool\Master_thesis\Project\3 - Implementation\1 - Data\1-BC\NOAA-ClimateDataOnline\3370204.csv'
+    # read the information in the file
+    data_obs_NOAA = pd.read_csv(path_file_NOAA)
+    # unit of PRCP are mm
+    # unit of temperature are degrees Celsius
+    
+    # add Year, month and season columns for graphs
+    data_obs_NOAA = add_year_month_season(data_obs_NOAA,'DATE')
+
+    return data_obs_NOAA
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
